f1e1add58f66910010ccd771d8efcb0f965ecf70
SajithJude <judesajith.aj@gmail.com>
2022-11-27 00:49:37 +0530

    dsds

--- app.py
+++ app.py
@@ -25,28 +25,33 @@ st.write('\n')
 
 st.title('Testing the Zoo Model')
 st.write('Test image')
-im = cv2.imread("assets/test_image1.jpeg")
+# im = cv2.imread("assets/test_image1.jpeg")
 # showing image
-st.image('assets/test_image1.jpeg')
+im = st.file_uploader("Choose an image")
+if im is not None:
+    st.image(im)
 
-#Then, we create a detectron2 config and a detectron2 `DefaultPredictor` to run inference on this image.
-cfg = get_cfg()
-cfg.MODEL.DEVICE = "cpu"
-# print(cfg)
-# add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
-cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
-# Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
-cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
-predictor = DefaultPredictor(cfg)
-outputs = predictor(im)
+    # To read file as bytes:
 
-st.write('Writing pred_classes/pred_boxes output')
-st.write(outputs["instances"].pred_classes)
-st.write(outputs["instances"].pred_boxes)
 
-st.write('Using Vizualizer to draw the predictions on Image')
-v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
-out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-st.image(out.get_image()[:, :, ::-1])
-Footer
+    #Then, we create a detectron2 config and a detectron2 `DefaultPredictor` to run inference on this image.
+    cfg = get_cfg()
+    cfg.MODEL.DEVICE = "cpu"
+    # print(cfg)
+    # add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
+    cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
+    # Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+    predictor = DefaultPredictor(cfg)
+    outputs = predictor(im)
+
+    st.write('Writing pred_classes/pred_boxes output')
+    st.write(outputs["instances"].pred_classes)
+    st.write(outputs["instances"].pred_boxes)
+
+    st.write('Using Vizualizer to draw the predictions on Image')
+    v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
+    out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+    st.image(out.get_image()[:, :, ::-1])
+# Footer
